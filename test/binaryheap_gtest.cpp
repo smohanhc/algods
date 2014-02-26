6a5341835cafb2fda09c6409fcccd4b86e925a75
sumodm <sumodm@g.clemson.edu>
2014-02-25 18:43:22 -0800

    Adding the non-fixtured version of the test.

--- test/binaryheap_gtest.cpp
+++ test/binaryheap_gtest.cpp
@@ -8,73 +8,27 @@
 using namespace std;
 
 namespace 
-{ //BinaryHeap<int> b1(5);
- class BHeapTest : public ::testing::Test
- { 
-   public:
-   BHeapTest()
-   { b1 = BinaryHeap<int>(5);
-     b2 = BinaryHeap<int>(1000);
-     b3 = BinaryHeap<float>(1000);
-     //b4 = BinaryHeap<long>(1000);
-   }
-   virtual ~BHeapTest(){}
+{ 
    
-   protected:
-   vector<int> data_;
-   vector<int> cdata_;
-   vector<float> fdata_;
-   vector<long> ldata_;
-   BinaryHeap<int> b1;
-   BinaryHeap<int> b2;
-   BinaryHeap<float> b3;
-   //BinaryHeap<long> b4;
-   
-   virtual void SetUp()
-   { // BHeap int : 5
-     for(int i=0; i<5; ++i)
-       data_.push_back(i);
-     b1.Heapify(data_.begin(), data_.end());
-
-     // BHeap int : 1000
-      for(int i=0; i<1000; ++i)
-       cdata_.push_back(i);
-     b2.Heapify(cdata_.begin(), cdata_.end());
-
-     //BHeap Float : 1000
-     vector<float> fdata_;
-     fdata_.clear(); 
-     for(int i=0; i<1000; ++i)
-       fdata_.push_back(i*pi);
-     b3.Heapify(fdata_.begin(), fdata_.end());
-   
-     // BHeap long : 1000000
-     /*vector<long> ldata_;
-     ldata_.clear(); 
-     for(long i=0; i<1000000; ++i)
-       ldata_.push_back(i);
-     b4.Heapify(ldata_.begin(), ldata_.end());*/
-   }
-   
-   virtual void TearDown()
-   { data_.clear();
-     cdata_.clear();
-     fdata_.clear();
-     ldata_.clear();
-   }
- };
-
+ // Test 1: Simple int test.
  TEST(BinHeapTest, SimpleTest)
- { BinaryHeap<int> B(5);
-   vector<int> data;
+ { vector<int> data;
    for(int i=0; i<5; ++i)
      data.push_back(i);
+ 
+   BinaryHeap<int> B(5);
    ASSERT_EQ(0, B.Heapify(data.begin(), data.end()));
  }
 
-
- TEST_F(BHeapTest, SimpleIntTest)
- { EXPECT_EQ(0, b1.ExtractXtrma());
+ // Test  2: BHeap int : 5
+ TEST(BHeapTest, SimpleIntTest)
+ { vector<int> data_;
+   for(int i=0; i<5; ++i)
+   data_.push_back(i);
+   
+   BinaryHeap<int> b1;
+   b1.Heapify(data_.begin(), data_.end());
+   EXPECT_EQ(0, b1.ExtractXtrma());
    EXPECT_EQ(1, b1.ExtractXtrma());	  
    EXPECT_EQ(2, b1.ExtractXtrma());	  
    EXPECT_EQ(3, b1.ExtractXtrma());	  
@@ -87,25 +41,48 @@ namespace
    EXPECT_EQ(4, b1.FindXtrma());	  
  }
 
- TEST_F(BHeapTest, LargeIntTest)
- { EXPECT_EQ(0, b2.FindXtrma());
+ // Test 3: BHeap int : 1000
+ TEST(BHeapTest, LargeIntTest)
+ { vector<int> cdata_;
+   for(int i=0; i<1000; ++i)
+     cdata_.push_back(i);
+   
+   BinaryHeap<int> b2;
+   b2.Heapify(cdata_.begin(), cdata_.end());
+   EXPECT_EQ(0, b2.FindXtrma());
    for(int i=0; i<10; ++i)
      b2.ExtractXtrma();
    EXPECT_EQ(10, b2.FindXtrma());
  } 
  
- TEST_F(BHeapTest, LargeFloatTest)
- { EXPECT_EQ(0, b3.FindXtrma());
+ // Test 4: BHeap Float : 1000
+ TEST(BHeapTest, LargeFloatTest)
+ { vector<float> fdata_;
+   for(int i=0; i<1000; ++i)
+     fdata_.push_back(i*pi);
+   
+   BinaryHeap<float> b3;
+   b3.Heapify(fdata_.begin(), fdata_.end());
+   EXPECT_EQ(0, b3.FindXtrma());
    for(int i=0; i<10; ++i)
      b3.ExtractXtrma();
    EXPECT_LT(31.410, b3.FindXtrma());
    EXPECT_GT(31.420, b3.FindXtrma());
  }
   
- /*TEST_F(BHeapTest, LargeLongTest)
- { EXPECT_EQ(0, b4.FindXtrma());
+ // Test 5: BHeap long : 1000000
+ /*
+ TEST(BHeapTest, LargeLongTest)
+ { 
+   vector<long> ldata_;
+   for(long i=0; i<1000000; ++i)
+     ldata_.push_back(i);
+   
+   BinaryHeap<long> b4;
+   b4.Heapify(ldata_.begin(), ldata_.end());
+   EXPECT_EQ(0, b4.FindXtrma());
    for(int i=0; i<10; ++i)
-     b3.ExtractXtrma();
+     b4.ExtractXtrma();
    EXPECT_EQ(10, b4.FindXtrma());
    EXPECT_EQ(10, b4.FindXtrma());
  }*/
